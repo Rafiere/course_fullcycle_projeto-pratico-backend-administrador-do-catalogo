604b9a8bd46615fc7a2609544041ed178a377bd5
Rafiere <rafapgbr@gmail.com>
2023-12-20 00:18:35 -0300

    feat: add test to validate category instantiation and category validation when name is null

--- domain/src/test/java/dev/piccodev/domain/CategoryTest.java
+++ domain/src/test/java/dev/piccodev/domain/CategoryTest.java
@@ -1,14 +1,56 @@
 package dev.piccodev.domain;
 
+import dev.piccodev.domain.category.Category;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CategoryTest {
 
     @Test
-    public void testNewCategory(){
-        Category category = new Category();
+    public void givenAValidParams_whenCallNewCategory_thenInstantiateACategory(){
 
-        Assertions.assertNotNull(category);
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveAnError(){
+
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate());
+
+        final var expectedErrorMessage = "'name' should not be null";
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).getMessage());
+
+        final var expectedErrorCount = 1;
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
     }
 }
